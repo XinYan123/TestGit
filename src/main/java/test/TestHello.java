9f2b9b6162bd9db1812277c35c05701beb181181
xinyan123456 <504591201@qq.com>
2020-05-23 19:02:24 +0800

    加入了hello输出

---
+++ src/main/java/test/TestHello.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @description:
+ * @author: Frankcrose
+ * @createDate: 2020/5/23 19:01
+ */
+public class TestHello {
+
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+
+}
